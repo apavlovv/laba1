456626066d667454a9a4e6522fba287776e3c251
apavlovv <127902257+apavlovv@users.noreply.github.com>
2023-03-15 12:10:55 +0200

    Update laba3.cs

--- laba3.cs
+++ laba3.cs
@@ -1,11 +1,11 @@
-﻿namespace laba
+namespace laba3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            List<int> list = new List<int> { 1, 54, -32, 52, 8232, 64, 9, 10 }; // Створює самий Ліст 
+            List<int> list = new List<int> { 1, 54, -32, 52, 8232, 64, 9, 10, 7, 77, 27, 11, 12, 52353, 777, 7777 };
 
             var newlist = from i in list
                           where i >= 10 && i < 100
